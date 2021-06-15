8da34493ef0e87d0ab6225b937b55fe1a812a59b
AdvikaPandey <69717028+AdvikaPandey@users.noreply.github.com>
2021-06-15 13:35:12 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -31,7 +31,7 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {isStatic:true});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {isStatic:true,restitution:0.4});
 	World.add(world, packageBody);
 
 	//Create a Ground
